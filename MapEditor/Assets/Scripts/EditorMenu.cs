e0f6c7a18c02c4d74b7a33625ff55d0ed1b1aa3e
Rak <asd>
2013-11-14 10:11:54 +0200

    asd

--- MapEditor/Assets/Scripts/EditorMenu.cs
+++ MapEditor/Assets/Scripts/EditorMenu.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using System.Collections;
+using System.IO;
+using System;
 
 public class EditorMenu : MonoBehaviour {
 	
 	public enum MenuType{MainMenu = 0, RedactorMenu = 1};
 	public MenuType currentMenu = MenuType.MainMenu;
-	public int nowTextureNumber = 1;
-	const int maxTexture = 6;
+	public int nowTextureNumber = 10;
+	const int maxTexture = 13;
 	string mapWidthString = "20";
 	string mapHeightString = "20";
 	int mapWidth;
@@ -16,20 +18,37 @@ public class EditorMenu : MonoBehaviour {
 	int[,] mapArray = new int[100,100];
 	Texture[] textures;
 	GameObject plane;
+	string mapString,reader1;
+	int i1;
 
 	
 	void SaveMap() {
 		
+	//	string mapString;
+		string path = "Maps/";
+		string name = "Map" + Time.time*7 + ".map";
+		
+		mapString=mapString+mapWidthString+" "+mapHeightString+" " + Environment.NewLine;
+		for (i=0;i<mapWidth;i++){
+			for (j=0;j<mapHeight;j++){ 
+				mapString=mapString+mapArray[i,j].ToString()+" ";
+			}
+			mapString=mapString+Environment.NewLine;
+		}
+		StreamWriter file = new StreamWriter(path + name);
+		file.WriteLine(mapString);
+		file.Close();
 	}
 	
 	void CreateMap(int width, int heigth) {
 		mapWidth = width;
 		mapHeight = heigth;
 		for(int mapI=1; mapI<=mapWidth; mapI++) for(int mapJ=1;mapJ<=mapHeight;mapJ++){
+			mapArray[mapI-1,mapJ-1]=10;
 			GameObject mapPlane = Resources.Load("Prefabs/Plane") as GameObject;
 			GameObject mapPlane_ = Instantiate (mapPlane) as GameObject;
 			mapPlane_.transform.position = new Vector3(mapI-mapWidth/2,mapJ-mapHeight/2,0);
-			mapPlane_.name = "Plane_" + mapI.ToString() + "x" + mapJ.ToString();
+			mapPlane_.name = "Plane_" + mapI.ToString() + "x" + mapJ.ToString()+ " ";
 		}
 	}
 	
@@ -41,16 +60,16 @@ public class EditorMenu : MonoBehaviour {
 		{
 		     reader=reader+map.text[i]; i=i+1;
 		}
-		int n = int.Parse(reader);
+		mapWidth = int.Parse(reader);
 		reader="";i=i+1;
 		while (map.text[i]==numbers[0]||map.text[i]==numbers[1]||map.text[i]==numbers[2]||map.text[i]==numbers[3]||map.text[i]==numbers[4]||map.text[i]==numbers[5]||map.text[i]==numbers[6]||map.text[i]==numbers[7]||map.text[i]==numbers[8]||map.text[i]==numbers[9])
 		{
 		     reader=reader+map.text[i]; i=i+1;
 		}
-		int m = int.Parse(reader);
+		mapHeight = int.Parse(reader);
 		reader="";i=i+2;
-		   for (k=0;k<n;k++){
-		   for (j=0;j<m;j++)
+		   for (k=0;k<mapWidth;k++){
+		   for (j=0;j<mapHeight;j++)
 		       {
 			   while (map.text[i]==numbers[0]||map.text[i]==numbers[1]||map.text[i]==numbers[2]||map.text[i]==numbers[3]||map.text[i]==numbers[4]||map.text[i]==numbers[5]||map.text[i]==numbers[6]||map.text[i]==numbers[7]||map.text[i]==numbers[8]||map.text[i]==numbers[9])
 		{
@@ -66,12 +85,12 @@ public class EditorMenu : MonoBehaviour {
 		textures[4] = Resources.Load("Textures/texture4")as Texture;
 		textures[5] = Resources.Load("Textures/texture5")as Texture;
 		textures[6] = Resources.Load("Textures/texture6")as Texture;
-		for (k=1;k<=n;k++)
-		   for (j=1;j<=m;j++){
+     	for (k=1;k<=mapWidth;k++)
+		   for (j=1;j<=mapHeight;j++){
 			GameObject plane_ = Instantiate(plane) as GameObject;
 		plane_.transform.position = new Vector3 (k,j,0);
 			plane_.renderer.material.mainTexture = textures[mapArray[j-1,k-1]];
-			plane_.name="Plane_"+j.ToString()+"x"+k.ToString();
+			plane_.name="Plane_"+j.ToString()+"x"+k.ToString()+" ";
 		}
 	}
 	
@@ -93,17 +112,25 @@ public class EditorMenu : MonoBehaviour {
 	}
 	
 	void ShowRedactorMenu(){
-		if(GUI.Button (new Rect(10,10,50,20),"Save")){}
+		if(GUI.Button (new Rect(10,10,50,20),"Save")){
+			SaveMap();
+		}
 		if(GUI.Button (new Rect(10,31,50,20),"Clear")){
-			for(int clearI = 0;clearI<=mapWidth; clearI++)
-				for(int clearJ = 0;clearJ<=mapWidth; clearJ++) {
-				GameObject.Find("Plane_"+(clearI+1)+"x" + (clearJ+1)).renderer.material.mainTexture = Resources.Load("Textures/texture" + nowTextureNumber.ToString()) as Texture;
+			for(int clearI = 0;clearI<mapWidth; clearI++)
+				for(int clearJ = 0;clearJ<mapHeight; clearJ++) {
+				GameObject.Find("Plane_"+(clearI+1)+"x" + (clearJ+1)+" ").renderer.material.mainTexture = Resources.Load("Textures/texture10") as Texture;
 		
 				}
 		}
-		if(GUI.Button (new Rect(10,52,20,20),"+")){ nowTextureNumber++;}
-		if(GUI.Button (new Rect(31,52,20,20),"-")){ nowTextureNumber--;}
-		nowTextureNumber = Mathf.Clamp(nowTextureNumber,1,6);
+		if(GUI.Button (new Rect(10,52,20,20),"+")){
+			nowTextureNumber++;
+			if(nowTextureNumber==0)nowTextureNumber++;
+		}
+		if(GUI.Button (new Rect(31,52,20,20),"-")){
+			nowTextureNumber--;
+			if(nowTextureNumber==0)nowTextureNumber--;
+		}
+		nowTextureNumber = Mathf.Clamp(nowTextureNumber,-9,13);
 		GUI.Box(new Rect(10,73,50,20),nowTextureNumber.ToString());
 	}
 	
@@ -114,6 +141,7 @@ public class EditorMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+	
 		float xAxisValue = Input.GetAxis("Horizontal");
     	float yAxisValue = Input.GetAxis("Vertical");
 		float zAxisValue = Input.GetAxis("Perspective");
@@ -125,9 +153,25 @@ public class EditorMenu : MonoBehaviour {
 		if(Input.GetMouseButton(0)){
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit)) {
+			if (Physics.Raycast(ray, out hit)) {i1=6;
 				Debug.Log(hit.collider.gameObject.name);
-				hit.collider.gameObject.renderer.material.mainTexture = Resources.Load("Textures/texture" + nowTextureNumber.ToString()) as Texture;
+				hit.collider.gameObject.renderer.material.mainTexture = Resources.Load("Textures/texture" + nowTextureNumber.ToString())as Texture;
+			 //-----
+				int x=0;
+				while (int.TryParse(hit.collider.gameObject.name[i1].ToString(),out x)) 
+				{
+		     reader=reader+hit.collider.gameObject.name[i1]; i1=i1+1;
+		}
+		int a = int.Parse(reader);
+		reader="";i1=i1+1;
+		while (int.TryParse(hit.collider.gameObject.name[i1].ToString(),out x))
+				{
+		     reader=reader+hit.collider.gameObject.name[i1]; i1=i1+1;
+		}
+		int b = int.Parse(reader);
+		reader ="";mapArray[a-1,b-1]=nowTextureNumber;
+				
+		//======
 			}
 		}
 	}
